bb6647c5562fde0e7bc8058e5ff5154454127623
Maksim Ostapenko <95276551+Spookyviking@users.noreply.github.com>
2022-08-14 16:38:11 +0300

    commit snova

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -24,6 +24,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'django_filters',
+    'debug_toolbar',
     'djoser',
     'colorfield',
     'users.apps.UsersConfig',
